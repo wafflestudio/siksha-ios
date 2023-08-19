4ec22dae47afbbb73da1ad17034162c04de9787c
박정헌 <impri@naver.om>
2023-08-19 23:58:26 +0900

    Asset 변경

--- Siksha/Community/BoardNameCell.swift
+++ Siksha/Community/BoardNameCell.swift
@@ -9,10 +9,10 @@ import Foundation
 import SwiftUI
 struct BoardNameCell: View{
     let selectedBackgroundColor = Color.init("MainThemeColor")
-    let unSelectedBackgroundColor =  Color(red:242/255, green:242/255, blue:242/255,opacity:1)
+    let unSelectedBackgroundColor =  Color.init("UnselectedBoardColor")
 
     let selectedFontColor = Color.white
-    let unSelectedFontColor = Color (red:183/255, green:183/255, blue:183/255,opacity: 1);
+    let unSelectedFontColor = Color.init("ReviewLowColor")
 
     
    @State var isSelected:Bool
@@ -20,7 +20,7 @@ struct BoardNameCell: View{
     
     var body: some View{
         Text(boardName)
-             .font(.custom("NanumSquareOTFR", size: 15))
+             .font(.custom("Inter-Regular", size: 15))
             .padding(EdgeInsets(top: 9, leading: 12, bottom: 9, trailing: 12))
             .background(isSelected ? selectedBackgroundColor : unSelectedBackgroundColor)
             .foregroundColor(isSelected ? selectedFontColor : unSelectedFontColor)
