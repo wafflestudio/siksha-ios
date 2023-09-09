8b507fcc4f78f60fe6aa40e9c0659dee64cad815
Chaehyun Park <c.park@snu.ac.kr>
2023-09-09 16:36:47 +0900

    모델 생성
    post, comment 생성

---
+++ Siksha/Community/CommunityPostViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CommunityPostViewModel.swift
+//  Siksha
+//
+//  Created by Chaehyun Park on 2023/08/06.
+//
+
+import Foundation
+
+class CommunityPostViewModel: ObservableObject {
+    @Published var Post
+}
---
+++ Siksha/Models/Comment.swift
@@ -0,0 +1,15 @@
+//
+//  Comment.swift
+//  Siksha
+//
+//  Created by Chaehyun Park on 2023/09/09.
+//
+
+import Foundation
+
+class Comment: Object {
+    var id: Int
+    var userId: Int
+    var createdAt: Date
+    var content: String
+}
