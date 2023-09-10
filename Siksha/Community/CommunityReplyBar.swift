7810b2c772c6aa5ca16c1acb515b925195a4eba3
김령교 <horizonspluto@gmail.com>
2023-09-10 17:47:13 +0900

    JTAppleCalendar 버전 업. 댓글 입력 창 완성됨.

---
+++ Siksha/Community/CommunityReplyBar.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityReplyBar.swift
+//  Siksha
+//
+//  Created by 김령교 on 8/27/23.
+//
+
+import SwiftUI
+
+struct CommunityReplyBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityReplyBar()
+}
