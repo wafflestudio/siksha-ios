c9bafd3d8fcf192f64d03776bc3b54b381ef9ab8
impri2 <impri@naver.om>
2023-12-03 00:06:46 +0900

    fix multipart networking

--- Siksha/Networking/AlamofireNetworking.swift
+++ Siksha/Networking/AlamofireNetworking.swift
@@ -10,12 +10,27 @@ import Alamofire
 
 final class AlamofireNetworking: NetworkModuleProtocol {
     func request<T: Decodable>(endpoint: SikshaAPI) -> AnyPublisher<T, Error> {
-        let request = AF.request(endpoint)
-        
+        var request:DataRequest
+        if(endpoint.multiPartFormDataNeeded){
+            request = AF.upload(multipartFormData: endpoint.multipartFormData!, with: endpoint)
+            
+
+        }
+        else{
+             request = AF.request(endpoint)
+
+        }
         return request
             .validate()
             .publishDecodable(type: T.self)
+           
             .value()
+            .handleEvents(receiveCompletion: {_ in
+                if let data = request.data {
+                    print(String(bytes:data,encoding:.utf8))
+                }
+            }
+                )
             .mapError { $0 as Error }
             .eraseToAnyPublisher()
     }
