5c02b0766c1d1189ad99ba91e13f9b585cb934b9
impri2 <impri@naver.om>
2023-12-02 23:57:29 +0900

    add submit post viewmodel

---
+++ Siksha/Community/CommunitySubmitPostViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  CommunitySubmitPostViewModel.swift
+//  Siksha
+//
+//  Created by 박정헌 on 2023/12/02.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+protocol CommunitySubmitPostViewModelType:ObservableObject{
+    var content:String{get set}
+    var title:String{get set}
+    var isAnonymous:Bool{get set}
+    var images:[UIImage]{get set}
+    var isSubmitted:Bool{get set}
+    var isErrorAlert:Bool{get set}
+    func submitPost()
+    
+}
+class CommunitySubmitPostViewModel:CommunitySubmitPostViewModelType{
+    private let communityRepository: CommunityRepositoryProtocol
+    private var cancellables = Set<AnyCancellable>()
+    private var boardId:Int
+    @Published var content = ""
+    @Published var title = ""
+    @Published var isAnonymous = false
+    @Published var images:[UIImage] = []
+    @Published var isSubmitted = false
+    @Published var isErrorAlert = false
+    init(boardId:Int,communityRepository: CommunityRepositoryProtocol) {
+        self.boardId = boardId
+        self.communityRepository = communityRepository
+        
+    }
+    
+    func submitPost(){
+        communityRepository.submitPost(boardId: boardId, title: title, content: content, images: images.map{image in image.pngData()!}).sink(receiveCompletion: {error in print(error)}, receiveValue: { [weak self] response in
+            self?.isSubmitted = true
+            print("submit:\(self?.isSubmitted)")
+            
+        }).store(in: &cancellables)
+        
+    }
+    
+    
+}
+
