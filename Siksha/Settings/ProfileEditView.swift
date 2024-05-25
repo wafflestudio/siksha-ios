099ddc886fce49c4a1068d154c36ac12fa1a04b0
JH713 <strider13@snu.ac.kr>
2024-05-26 00:18:46 +0900

    프로필 편집 UI 수정

--- Siksha/Settings/ProfileEditView.swift
+++ Siksha/Settings/ProfileEditView.swift
@@ -15,12 +15,17 @@ struct ProfileEditView<ViewModel>: View where ViewModel:ProfileEditViewModel {
     var body: some View {
         VStack {
             profileImage.padding(.top, 65.0)
-                
+                .onTapGesture {
+                    print("Image tapped")
+                }
             nickname.padding(.top, 15.0)
-            
+            buttons.padding(.top, 18.0)
             Spacer()
-            
             versionInfo.padding(.bottom, 41.0)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            UIApplication.shared.endEditing()
         }
         .customNavigationBar(title: "프로필 관리")
         .navigationBarItems(leading: backButton)
@@ -34,32 +39,43 @@ struct ProfileEditView<ViewModel>: View where ViewModel:ProfileEditViewModel {
     }
     
     var nickname: some View {
-        ZStack(alignment: .trailing) {
-            ZStack(alignment: .center) {
-                RoundedRectangle(cornerRadius: 11)
-                    .strokeBorder(lineWidth: 1)
-                    .frame(width: 336, height: 49)
-                    .foregroundColor(Color("TextFieldBorderColor"))
-                TextField("닉네임", text: $viewModel.nickname)
-                    .multilineTextAlignment(.center)
-                    .frame(width: 137)
-                    .font(.custom("NanumSquareOTFB", size: 15))
+        RoundedRectangle(cornerRadius: 11)
+            .strokeBorder(lineWidth: 1)
+            .frame(width: 336, height: 49)
+            .foregroundColor(Color("TextFieldBorderColor"))
+            .overlay(
+                ClearableTextField("닉네임", text: $viewModel.nickname)
+                    .padding(.horizontal, 18)
+            )
+    }
+    
+    var buttons: some View {
+        HStack {
+            Button(action: {
+                print("cancel button tapped")
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(Color("LightGrayColor"))
+                        .frame(width: 164, height: 44)
+                    Text("취소")
+                        .font(.custom("NanumSquareOTFB", size: 14))
+                        .foregroundColor(Color.white)
+                }
             }
             Button(action: {
-                print("nickname edit button tapped")
-                    }) {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 8.0)
-                                .fill(Color("MainThemeColor"))
-                                .frame(width: 52, height: 33)
-                            Text("완료")
-                                .font(.custom("NanumSquareOTFR", size: 15))
-                                .foregroundColor(Color.white)
-                        }
-                    }
-                    .padding(.trailing, 8)
+                print("done button tapped")
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(Color("MainThemeColor"))
+                        .frame(width: 164, height: 44)
+                    Text("완료")
+                        .font(.custom("NanumSquareOTFB", size: 14))
+                        .foregroundColor(Color.white)
+                }
+            }
         }
-        
     }
     
     var versionInfo: some View {
@@ -81,6 +97,48 @@ struct ProfileEditView<ViewModel>: View where ViewModel:ProfileEditViewModel {
                 .resizable()
                 .frame(width: 7, height: 15)
         }
+    }
+}
+
+struct ClearableTextField: View {
+    var title: String
+    @Binding var text: String
+    
+    init(_ title: String, text: Binding<String>) {
+        self.title = title
+        _text = text
+    }
+    
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            TextField(title, text: $text)
+                .multilineTextAlignment(.center)
+                .font(.custom("NanumSquareOTFB", size: 15))
+                .padding(.horizontal, 20)
+            Image(systemName: "xmark.circle.fill")
+                .frame(width: 18, height: 18)
+                .foregroundColor(Color("LightGrayColor"))
+                .onTapGesture {
+                    text = ""
+                }
+        }
+    }
+}
+
+extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
 
