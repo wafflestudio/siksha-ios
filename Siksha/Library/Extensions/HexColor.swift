225ce4ee7f2a7b764707a32c2e2d148ed03012e1
Chaehyun Park <c.park@snu.ac.kr>
2024-08-10 23:40:57 +0900

    add hex color initializer

---
+++ Siksha/Library/Extensions/HexColor.swift
@@ -0,0 +1,18 @@
+//
+//  HexColor.swift
+//  Siksha
+//
+//  Created by Chaehyun Park on 8/10/24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
