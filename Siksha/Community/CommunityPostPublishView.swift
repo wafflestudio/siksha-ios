7478b581c6a052d187394333455239dd1251cca0
Chaehyun Park <c.park@snu.ac.kr>
2024-08-11 02:26:41 +0900

    reflect PostPublishView design changes

--- Siksha/Community/CommunityPostPublishView.swift
+++ Siksha/Community/CommunityPostPublishView.swift
@@ -18,6 +18,7 @@ struct CommunityPostPublishView<ViewModel>: View where ViewModel:CommunityPostPu
     @Binding var needRefresh:Bool
     @State private var isShowingPhotoLibrary = false
  //   @State private var addedImages = [UIImage]()
+    private var cornerRadius = 7.0
     @ObservedObject  var viewModel:ViewModel
     private var cancellables = Set<AnyCancellable>()
     init( needRefresh: Binding<Bool> , viewModel: ViewModel) {
@@ -42,46 +43,63 @@ struct CommunityPostPublishView<ViewModel>: View where ViewModel:CommunityPostPu
             viewModel.submitPost()
         }) {
             ZStack {
-                RoundedRectangle(cornerRadius: 15.0)
-                    .fill(Color.white)
-                    .frame(width: 45, height: 25)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill(viewModel.title.isEmpty || viewModel.content.isEmpty ? Color.gray : Color("MainThemeColor"))
+                    .frame(height: 60)
                 Text("올리기")
-                    .font(.custom("Inter-SemiBold", size: 12))
-                    .foregroundColor(Color.init("MainThemeColor"))
+                    .font(.custom("Inter-SemiBold", size: 17))
+                    .foregroundColor(Color.white)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .disabled(viewModel.title.isEmpty || viewModel.content.isEmpty)
+    }
+
+    var anonymousButton: some View {
+        Toggle(isOn: $viewModel.isAnonymous) {
+            Text("익명")
+                .font(.custom("Inter-Regular", size: 14))
+        }
+        .toggleStyle(CustomCheckboxStyle())
+    }
+    
+    struct CustomCheckboxStyle: ToggleStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(configuration.isOn ? Color.clear : Color(hex:0xDFDFDF), lineWidth: 1)
+                        .frame(width: 14, height: 14)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(configuration.isOn ? Color("MainThemeColor") : Color.clear)
+                        )
+
+
+                    if configuration.isOn {
+                        Image(systemName: "checkmark")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.white)
+                            .frame(width: 10, height: 10)
+                    }
+                }
+                .onTapGesture {
+                    configuration.isOn.toggle()
+                }
+
+                Text("익명")
+                    .font(.custom("Inter-Regular", size: 14))
+                    .foregroundColor(configuration.isOn ? Color("MainThemeColor") : Color(hex:0x575757))
             }
         }
     }
-    
-    var anonymousButton: some View {
-        Button(action: {
-            viewModel.isAnonymous.toggle()
-                }) {
-                    if viewModel.isAnonymous {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15.0)
-                                .fill(Color("MainThemeColor"))
-                                .frame(width: 34, height: 25)
-                            Text("익명")
-                                .font(.custom("Inter-SemiBold", size: 12))
-                                .foregroundColor(Color.white)
-                        }
-                    } else {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15.0)
-                                .stroke(Color("MainThemeColor"))
-                                .frame(width: 34, height: 25)
-                            Text("익명")
-                                .font(.custom("Inter-SemiBold", size: 12))
-                                .foregroundColor(Color("MainThemeColor"))
-                        }
-                    }
-                }
-    }
+
     
     var customDivider: some View {
         HStack {
-            Color("ReviewLowColor") 
-                .frame(height: 1)
+            Color("DefaultImageColor")
+                .frame(height: 0.5)
                 .frame(maxWidth: .infinity)
         }
     }
@@ -90,13 +108,42 @@ struct CommunityPostPublishView<ViewModel>: View where ViewModel:CommunityPostPu
         VStack(spacing: 0) {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
+                    ForEach(viewModel.images, id: \.self) { image in
+                        ZStack(alignment: .topTrailing) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .renderingMode(.original)
+                                .scaledToFill()
+                                .frame(width: 106, height: 106)
+                                .clipped()
+                                .cornerRadius(cornerRadius)
+                            
+                            Button(action: {
+                                viewModel.removeImage(image)
+                            }) {
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(Color(hex:0x575757))
+                                        .frame(width: 24, height: 24)
+                                    
+                                    Image(systemName: "xmark")
+                                        .resizable()
+                                        .foregroundColor(Color(hex:0xDFDFDF))
+                                        .frame(width: 10, height: 10)
+                                }
+                                .offset(x: 8, y: -8)
+                            }
+                        }
+                        .frame(width:115, height: 130)
+                    }
+                    
                     Button(action: {
                         self.isShowingPhotoLibrary = true
                     }) {
                         ZStack {
-                            Rectangle()
+                            RoundedRectangle(cornerRadius: cornerRadius)
                                 .foregroundColor(Color("DefaultImageColor"))
-                                .frame(width: 118, height: 118)
+                                .frame(width: 106, height: 106)
 
                             Image(systemName: "plus")
                                 .resizable()
@@ -107,61 +154,31 @@ struct CommunityPostPublishView<ViewModel>: View where ViewModel:CommunityPostPu
                     .sheet(isPresented: $isShowingPhotoLibrary) {
                         ImagePickerCoordinatorView(selectedImages: $viewModel.images, maxSelection: 5)
                     }
-                    
-                    ForEach(viewModel.images, id: \.self) { image in
-                        ZStack(alignment: .topTrailing) {
-                            Image(uiImage: image)
-                                .resizable()
-                                .renderingMode(.original)
-                                .scaledToFill()
-                                .frame(width: 119, height: 119)
-                                .clipped()
-                                .padding([.top, .trailing], 5)
-                            
-                            Button(action: {
-                                viewModel.removeImage(image)
-                            }) {
-                                ZStack {
-                                    Circle()
-                                        .foregroundColor(Color("MainThemeColor"))
-                                        .frame(width: 24, height: 24)
-                                    
-                                    Image(systemName: "xmark")
-                                        .resizable()
-                                        .foregroundColor(Color.white)
-                                        .frame(width: 10, height: 10)
-                                }
-                                .padding(EdgeInsets(top: 0, leading: 8, bottom: 8, trailing: 0))
-                            }
-                        }
-                    }
+
                 }
             }
-            .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
         }
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                HStack {
-                    anonymousButton
-                    Spacer()
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: cornerRadius)
+                        .fill(Color(hex: 0xF8F8F8))
+                        .frame(height: 35)
+                    TextField("제목", text: $viewModel.title)
+                        .font(.custom("Inter-Bold", size: 14))
+                        .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
                 }
-                .padding(EdgeInsets(top: 0, leading: 7, bottom: 7, trailing: 0))
-                
-                TextField("제목", text: $viewModel.title)
-                    .font(.custom("Inter-Bold", size: 14))
-                    .padding(EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12))
-                
-                customDivider
-                
-                ZStack {
+                .frame(maxWidth:.infinity)
+                                
+                ZStack(alignment: .topLeading) {
                     let placeholder: String = "내용을 입력하세요."
                     
                     TextEditor(text: $viewModel.content)
                         .font(.custom("Inter-ExtraLight", size: 12))
-                        .frame(minHeight: 30)
+                        .frame(minHeight: 120)
                         .fixedSize(horizontal: false, vertical: true)
                     
                     if viewModel.content.isEmpty {
@@ -169,20 +186,29 @@ struct CommunityPostPublishView<ViewModel>: View where ViewModel:CommunityPostPu
                             .font(.custom("Inter-ExtraLight", size: 12))
                             .foregroundColor(Color.init("ReviewLowColor"))
                             .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading, 4)
+                            .padding(EdgeInsets(top: 8, leading: 6, bottom: 0, trailing: 0))
                     }
                 }
-                .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+                .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                HStack {
+                    anonymousButton
+                    Spacer()
+                }
                 
                 customDivider
                 
                 imageSection
                 
                 Spacer()
+
+                postButton
+                    .padding(.bottom, 5)
+                
             }
             .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
             .customNavigationBar(title: "글쓰기")
-            .navigationBarItems(leading: backButton, trailing: postButton)
+            .navigationBarItems(leading: backButton)
             
         }.navigationBarBackButtonHidden(true)
         .alert(isPresented: $viewModel.isErrorAlert, content: {
