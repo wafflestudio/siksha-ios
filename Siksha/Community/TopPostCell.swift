e707373431712aba309cf02ce8a8d508928fe94b
impri2 <impri@naver.om>
2024-08-11 14:57:22 +0900

    minor design changes

--- Siksha/Community/TopPostCell.swift
+++ Siksha/Community/TopPostCell.swift
@@ -14,16 +14,20 @@ struct TopPostCell:View{
         NavigationLink(destination: CommunityPostView(viewModel: CommunityPostViewModel(communityRepository: DomainManager.shared.domain.communityRepository, postId: post.id), needPostViewRefresh:needRefresh)) {
             HStack(alignment: .center){
                 Text(post.title)
-                    .font(.custom("NanumSquareOTFB", size: 12))
+                    .font(.custom("NanumSquareOTFBold", size: 12))
+                    .foregroundColor(.black)
+                    .lineLimit(1)
                 Spacer().frame(width:15)
                 Text(post.content)
-                    .font(.custom("NanumSquareOTFR",size:12))
+                    .font(.custom("NanumSquareOTFRegular",size:12))
+                    .foregroundColor(Color("ReviewHighColor"))
+                    .lineLimit(1)
                 Spacer()
                 
                 Image("like").padding(EdgeInsets(top: 0, leading: 0, bottom: 2, trailing: 0))
                 
                 Text("\(post.likeCount)").foregroundColor(Color.init(red:1,green:149/255,blue:34/255))
-                    .font(.custom("Inter-ExtraLight",size:8))
+                    .font(.custom("NanumSquareOTFRegular",size:8))
             }
             
             .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
