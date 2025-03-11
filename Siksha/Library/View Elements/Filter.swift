1b6e1f980f572eb47c3a892627ea66dae066d8d3
h9kwon <harryk0702@gmail.com>
2025-03-12 00:01:57 +0900

    fix: use regular font weight for unselected filter's label

--- Siksha/Library/View Elements/Filter.swift
+++ Siksha/Library/View Elements/Filter.swift
@@ -5,18 +5,24 @@
 //  Created by 박정헌 on 1/12/25.
 //
 import SwiftUI
-struct FilterItem:View{
+
+struct FilterItem: View {
     var text: String
     var isOn: Bool
     var isCheck: Bool
-    var body:some View{
-        HStack(spacing:2){
-            if(isOn && isCheck){
+    var body:some View {
+        HStack(spacing: 2) {
+            if (isOn && isCheck) {
                 Image("FilterCheck")
             }
-            Text(text)
-                .font(.custom("NanumSquareOTFB", size: 13))
-            if(!isCheck){
+            if (isOn) {
+                Text(text)
+                    .font(.custom("NanumSquareOTFB", size: 13))
+            } else {
+                Text(text)
+                    .font(.custom("NanumSquareOTF", size: 13))
+            }
+            if (!isCheck) {
                 Image("select")
             }
         }.padding(EdgeInsets(top: 9, leading: 10, bottom: 10, trailing: 9))
@@ -26,8 +32,6 @@ struct FilterItem:View{
                 RoundedRectangle(cornerRadius: 30)
                     .stroke(isOn ? Color("MainThemeColor") : Color("FilterUnselectedBorderColor"), lineWidth: 1)
             )
-
-       
     }
 }
 
