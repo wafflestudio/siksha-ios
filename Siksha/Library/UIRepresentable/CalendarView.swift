57adef9868849ba99fa47087a14478d745de3c44
impri2 <impri@naver.om>
2025-08-17 19:01:19 +0900

    fix: fix images not showing

--- Siksha/Library/UIRepresentable/CalendarView.swift
+++ Siksha/Library/UIRepresentable/CalendarView.swift
@@ -146,7 +146,7 @@ class DateHeader: JTACMonthReusableView {
         
         leftButton.frame = CGRect(x: 0, y: 0, width: 10, height: 16)
         rightButton.frame = CGRect(x: 0, y: 0, width: 10, height: 16)
-        leftButton.setBackgroundImage(UIImage(named: "PrevDate-orange"), for: .normal)
+        leftButton.setBackgroundImage(UIImage(named: "Prevdate-orange"), for: .normal)
         rightButton.setBackgroundImage(UIImage(named: "NextDate-orange"), for: .normal)
         
         super.init(frame: frame)
