9943bb62b9be480215009481c87edabc3bc1ba37
Sumin Lee <cirtuare@gmail.com>
2025-10-12 04:50:46 +0900

    chore: fix HTTPHeaderField to "Authorization" (#43)

--- Siksha/Library/Extensions/Extensions.swift
+++ Siksha/Library/Extensions/Extensions.swift
@@ -112,8 +112,8 @@ extension UIApplication {
 }
 
 extension URLRequest {
-    mutating func setToken(token: String, type: String = "authorization") {
-        self.setValue("Bearer " + token, forHTTPHeaderField: "\(type)-token")
+    mutating func setToken(token: String, type: String = "Authorization") {
+        self.setValue("Bearer " + token, forHTTPHeaderField: type)
     }
 }
 
