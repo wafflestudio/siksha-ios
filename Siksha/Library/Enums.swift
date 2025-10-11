d33c7dae7743dfe6d55c22474ce19db363ee40af
h9kwon <harryk0702@gmail.com>
2025-10-12 00:20:35 +0900

    chore: delete outdated breakfast/lunch/dinner icon images and update image string

--- Siksha/Library/Enums.swift
+++ Siksha/Library/Enums.swift
@@ -28,17 +28,17 @@ struct TypeInfo: Identifiable {
 
         switch(type){
         case .breakfast:
-            self.icon = "Breakfast"
+            self.icon = "BreakfastTime"
             self.width = 20
             self.height = 12
             self.name = "아침"
         case .lunch:
-            self.icon = "Lunch"
+            self.icon = "LunchTime"
             self.width = 20
             self.height = 20
             self.name = "점심"
         case .dinner:
-            self.icon = "Dinner"
+            self.icon = "DinnerTime"
             self.width = 14
             self.height = 14
             self.name = "저녁"
