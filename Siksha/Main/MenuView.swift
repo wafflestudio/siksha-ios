5dccd446f36e430ef5967ca1dc7045c2a7ac682a
이지현 <strider13@snu.ac.kr>
2026-01-07 12:04:59 +0900

    chore: delete useless annotation

--- Siksha/Main/MenuView.swift
+++ Siksha/Main/MenuView.swift
@@ -70,17 +70,6 @@ struct MenuView: View {
         .alert(isPresented: $viewModel.showNetworkAlert, content: {
             Alert(title: Text("식단"), message: Text("식단을 받아오지 못했습니다. 이전에 불러왔던 식단으로 대신 표시합니다."), dismissButton: .default(Text("확인")))
         })
-//        .onAppear {
-//            if viewModel.reloadOnAppear {
-//                viewModel.getMenu(date: viewModel.selectedDate)
-//            } else {
-//                viewModel.reloadOnAppear = true
-//            }
-//            viewModel.loadFilters()
-//        }
-//        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-//            viewModel.getMenu(date: viewModel.selectedDate)
-//        }
         .sheet(isPresented: isFilterModalPresented){
             MenuFilterView(menuViewModel: viewModel, menuFilterType: selectedFilterType ?? .all)
                 .presentationDetents([.height(selectedModalHeight)])
