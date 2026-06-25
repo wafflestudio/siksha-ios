787e96662d013525f7ba61df3f777ac5db1961be
h9kwon <harryk0702@gmail.com>
2026-06-25 21:58:11 +0900

    chore: delete unused functions

--- Siksha/Data/Network/Networking.swift
+++ Siksha/Data/Network/Networking.swift
@@ -7,12 +7,9 @@
 
 import Foundation
 import Alamofire
-import Combine
-import SwiftyJSON
 
 class Networking {
     static let shared = Networking()
-    private var cancellables = Set<AnyCancellable>()
     
     private init() {}
     
@@ -31,136 +28,5 @@ class Networking {
         let request = AF.request(SikshaAPI.refreshAccessToken(token: token))
         
         return request.validate().publishData()
-    }
-    
-    func getFestivalDates() -> DataResponsePublisher<FestivalDatesResponse> {
-        let request = AF.request(SikshaAPI.getFestivalDates)
-        return request.validate().publishDecodable(type: FestivalDatesResponse.self, decoder: JSONDecoder())
-    }
-
-    func getMenus(startDate: String, endDate: String, noMenuHide: Bool) -> DataResponsePublisher<Data> {
-        let request = AF.request(SikshaAPI.getMenus(startDate: startDate, endDate: endDate, noMenuHide: noMenuHide))
-        
-        return request.validate().publishData()
-    }
-    func getMenuFromId(menuId:Int)->DataResponsePublisher<MenuIdResponse>{
-        let request = AF.request(SikshaAPI.getMenuFromId(menuId: menuId))
-        return request.validate().publishDecodable(type:MenuIdResponse.self,decoder: JSONDecoder())
-    }
-    func likeReview(reviewId: Int)->AnyPublisher<Void, AppError>{
-        let request = AF.request(SikshaAPI.likeReview(reviewId: reviewId))
-        return request
-            .validate()
-            .publishData(emptyResponseCodes: [204])
-            .tryMap { response in
-                if let error = response.error {
-                    if let data = response.data,
-                       let message = try? JSON(data: data)["message"].stringValue {
-                        throw AppError.error(message)
-                    }
-                    switch error {
-                    case .responseSerializationFailed(let reason):
-                        if case .inputDataNilOrZeroLength = reason {
-                            return ()
-                        }
-                    default: break
-                    }
-                    throw error
-                }
-                return ()
-            }
-            .mapError({ error in
-                if let e = error as? AppError {
-                    return e
-                }
-                if let afError = error as? AFError {
-                    return .serverError("\(afError.responseCode ?? 0)", afError.localizedDescription)
-                }
-                return .unknownError("unknown error occurred")
-            })
-            .eraseToAnyPublisher()
-    }
-    
-    func unlikeReview(reviewId: Int)->AnyPublisher<Void, AppError>{
-        let request = AF.request(SikshaAPI.unlikeReview(reviewId: reviewId))
-        return request
-            .validate(statusCode: 200..<300)
-            .publishData(emptyResponseCodes: [204])
-            .tryMap { response in
-                if let error = response.error {
-                    throw error
-                }
-                return ()
-            }
-            .mapError({ error in
-                if let afError = error as? AFError {
-                    return .serverError("\(afError.responseCode ?? 0)", afError.localizedDescription)
-                }
-                return .unknownError("unknown error occurred")
-            })
-            .eraseToAnyPublisher()
-    }
-    func getRestaurants() -> DataResponsePublisher<Data> {
-        let request = AF.request(SikshaAPI.getRestaurants)
-        
-        return request.validate().publishData()
-    }
-    
-    func getScoreDistribution(menuId: Int) -> DataResponsePublisher<ScoreDistributionResponse> {
-        let request = AF.request(SikshaAPI.getScoreDistribution(menuId: menuId))
-        
-        return request.validate().publishDecodable(type: ScoreDistributionResponse.self)
-    }
-    
-    func getKeywordDistribution(menuId: Int) -> DataResponsePublisher<KeywordDistributionResponse> {
-        let request = AF.request(SikshaAPI.getKeywordDistribution(menuId: menuId))
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        return request.validate().publishDecodable(type: KeywordDistributionResponse.self, decoder: decoder)
-    }
-    
-    func getCommentRecommendation(score: Int) -> DataResponsePublisher<CommentRecommendationResponse> {
-        let request = AF.request(SikshaAPI.getCommentRecommendation(score: score))
-        
-        return request.validate().publishDecodable(type: CommentRecommendationResponse.self)
-    }
-    
-    func submitReview(menuId: Int, score: Int, comment: String, taste: String, price: String, foodComposition: String) -> DataResponsePublisher<Data> {
-        let request = AF.request(SikshaAPI.submitReview(menuId: menuId, score: score, comment: comment, taste: taste, price: price, foodComposition: foodComposition))
-        
-        return request.validate().publishData()
-    }
-    
-    func submitReviewImages(menuId: Int, score: Int, comment: String, taste: String, price: String, foodComposition: String, images: [Data]) -> DataResponsePublisher<Data> {
-        let api = SikshaAPI.submitReviewImages(menuId: menuId, score: score, comment: comment, taste: taste, price: price, foodComposition: foodComposition, images: images)
-        let request = AF.upload(multipartFormData: api.multipartFormData!, with: api)
-        
-        return request.validate().publishData()
-    }
-    func getUserInfo() -> DataResponsePublisher<UserInfoResponse> {
-        let request = AF.request(SikshaAPI.getUserInfo)
-        
-        return request.validate().publishDecodable(type: UserInfoResponse.self)
-    }
-    
-    func editReview(reviewId: Int, menuId: Int, score: Int, comment: String, taste: String, price: String, foodComposition: String, images: [Data]?) -> DataResponsePublisher<Data> {
-        let endpoint = SikshaAPI.editReview(reviewId: reviewId, menuId: menuId, score: score, comment: comment, taste: taste, price: price, foodComposition: foodComposition, images: images)
-        
-        return AF.upload(multipartFormData: endpoint.multipartFormData!, with: endpoint)
-            .validate()
-            .publishData()
-    }
-    
-    func submitVOC(comment: String, platform: String) -> DataResponsePublisher<Data> {
-        let request = AF.request(SikshaAPI.submitVOC(comment: comment, platform: platform))
-        
-        return request.validate().publishData()
-    }
-}
-
-struct EmptyEntity: Codable, EmptyResponse {
-    static func emptyValue() -> EmptyEntity {
-        return EmptyEntity.init()
     }
 }
